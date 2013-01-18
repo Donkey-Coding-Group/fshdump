9229f9813591f38a0bba47c60c77ca577f4208e8
Niklas Rosenstein <rosensteinniklas@googlemail.com>
2013-01-18 19:59:06 +0100

    Added FSHDumpWriterMain

---
+++ src/FSHDumpWriterMain.java
@@ -0,0 +1,21 @@
+/**
+ * This main class implements a command-line utility for writing
+ * an fshdump. Command-line options are:
+ *
+ * [-s]:            Show statistics (number of files/directories, bytes
+ *                  written, execution time).
+ * [-f FORMAT]:     Rendering format. strict|readable
+ * [-d DIRNAME]:    Name of the directory to create the dump from.
+ *                  Defaults to the current working directory.
+ * [-o FILENAME]:   Output filename or stdout|null.
+ * [-n LINEENDING]: Line ending for readable formatting. cr|lf|crlf
+ */
+public class FSHDumpWriterMain {
+
+    public static void main(String[] args) {
+        // TODO: implement
+        System.exit(-1);
+    }
+
+}
+
